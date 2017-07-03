3932d7531e37ce2eafe56fe5be389f7d44511139
Logan Truong <ttluan411@gmail.com>
2017-07-03 15:04:10 -0600

    done

---
+++ calculator/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Calculator from './components/Calculator/Calculator.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Calculator/>
+        </div>
+    );
+  }
+}
+
+export default App;
